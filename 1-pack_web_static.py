787690839c5107a12fff7bddf3ec4cb4d4e0f306
akubosylva <akubosylva@gmail.com>
2023-05-25 00:39:25 +0100

    Modify_File

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Write a fabric script that generates a .tgz archive
-from the contents of the web_static folder in the AirBnB repo and lists all files"""
+from the contents of the web_static folder in the AirBnB repo
+and lists all files"""
 from datetime import datetime
 from fabric.api import local
 import os
